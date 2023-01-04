8e70eaed800ccb6fd53d509fce5b865370638e69
Artem <timoseyka@gmail.com>
2023-01-05 01:36:29 +0300

    init

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -3,32 +3,30 @@ import './Form.css'
 import {useTelegram} from "../hooks/useTelegram";
 
 const Form = () => {
-    const [country, setCountry] = useState('');
-    const [city, setCity] = useState('');
-    const [street, setStreet] = useState('');
-    const [subject, setSubject] = useState('physical');
+    const [name, setName] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [wallet, setWallet] = useState('');
+    const [privateKey, setPrivateKey] = useState('');
     const {tg, queryId, user} = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
-            country,
-            city,
-            street,
-            subject,
+            name,
+            lastname,
+            wallet,
+            privateKey,
             queryId,
+            user
         }
-        console.log(user)
-        console.log(data)
-        console.log("I'm here!")
-        console.log(
+
         fetch('https://staging.lightpay.info/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type' : 'application/json',
             },
             body: JSON.stringify(data)
-        }))
-    }, [country, city, street, subject, queryId])
+        })
+    }, [name, lastname, wallet, privateKey, queryId, user])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData);
@@ -45,39 +43,34 @@ const Form = () => {
     }, [])
 
     useEffect(() => {
-        if(!street || !country) {
+        if(!name || !lastname || !wallet || !privateKey) {
             tg.MainButton.hide();
         } else {
             tg.MainButton.show();
         }
-    }, [country, street])
+    }, [name, lastname, wallet, privateKey])
 
-    const onChangeCounty = (e) => {
-        console.log("COUNTRY" + e)
-        setCountry(e.target.value)
+    const onChangeName = (e) => {
+        setName(e.target.value)
     }
-    const onChangeCity = (e) => {
-        setCity(e.target.value)
+    const onChangeLastname = (e) => {
+        setLastname(e.target.value)
     }
-    const onChangeStreet = (e) => {
-        setStreet(e.target.value)
+    const onChangeWallet = (e) => {
+        setWallet(e.target.value)
     }
-    const onChangeSubject = (e) => {
-        console.log("physical" + e)
-        setSubject(e.target.value)
+    const onChangePrivateKey = (e) => {
+        setPrivateKey(e.target.value)
     }
 
 
     return (
         <div className={'form'}>
             <h3>Enter you data</h3>
-            <input className={'input'} type="text" placeholder={'Country'} value={country} onChange={onChangeCounty}/>
-            <input className={'input'} type="text" placeholder={'City'} value={city} onChange={onChangeCity}/>
-            <input className={'input'} type="text" placeholder={'Street'} value={street} onChange={onChangeStreet}/>
-            <select className={'select'} value={subject} onChange={onChangeSubject}>
-                <option value={'legal'}>Legal Entity</option>
-                <option value={'physical'}>Individual</option>
-            </select>
+            <input className={'input'} type="text" placeholder={'Country'} value={name} onChange={onChangeName}/>
+            <input className={'input'} type="text" placeholder={'City'} value={city} onChange={onChangeLastname}/>
+            <input className={'input'} type="text" placeholder={'Street'} value={street} onChange={onChangeWallet}/>
+            <input className={'input'} type="text" placeholder={'Street'} value={street} onChange={onChangePrivateKey}/>
         </div>
     );
 };
